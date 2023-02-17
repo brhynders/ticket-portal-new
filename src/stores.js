e9b7af333f15274be131dfdb141927992ba36d15
Brandon <brandonmrhynders@gmail.com>
2023-02-16 21:07:26 -0500

    Auto subscription to pocketbase auth model

--- src/stores.js
+++ src/stores.js
@@ -2,5 +2,6 @@ import { writable } from "svelte/store";
 import Pocketbase from "pocketbase";
 
 export const pb = new Pocketbase("https://tickets.elegantwebcreations.com");
-export const user = writable(false);
-if (pb.authStore.isValid) user.set(pb.authStore.model);
+export const user = writable(pb.authStore.model);
+
+pb.authStore.onChange(auth => user.set(pb.authStore.model));
